8b00801168064b66d666a3f1df1cb4e72c3e6b73
mla <admin@mla.xin>
2019-10-17 17:07:26 +0800

    Signed-off-by: mla <admin@mla.xin>

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,7 +5,7 @@
         <div class="row justify-content-lg-center">
           <div class="col-12">
             <div class="text-xs text-muted  border-top border-light py-4 mt-4">
-              <span class="copyright"> © 2018 MLA.xin <a href="http://beian.miit.gov.cn">冀ICP备18034189号</a> </span>
+              <span class="copyright"> © 2015-{{ year }} MLA.xin <a href="http://beian.miit.gov.cn">冀ICP备18034189号</a> </span>
             </div>
           </div>
         </div>
@@ -15,6 +15,15 @@
 </template>
 <script>
 export default {
-  name: 'Footer'
+  name: 'Footer',
+  data () {
+    return {
+      year: ''
+    }
+  },
+  created () {
+    const date = new Date()
+    this.year = date.getFullYear()
+  }
 }
 </script>
